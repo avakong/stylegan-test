35a3c8df9cc262d51c326c9be5e77a27cb6c7cd1
Marian <mail@marian42.de>
2019-12-09 10:10:15 +0100

    Interpolate in the style domain

--- create_image_sequence.py
+++ create_image_sequence.py
@@ -17,18 +17,26 @@ standard_normal_distribution = torch.distributions.normal.Normal(0, 1)
 RESOLUTION = 256
 STEP = int(math.log(RESOLUTION, 2)) - 2
 
-SAMPLE_COUNT = 20 # Number of distinct objects to generate and interpolate between
-TRANSITION_FRAMES = 1800 // SAMPLE_COUNT
+DURATION_IN_SECONDS = 60
+SAMPLE_COUNT = 30 # Number of distinct objects to generate and interpolate between
+TRANSITION_FRAMES = DURATION_IN_SECONDS * 30 // SAMPLE_COUNT
 
 LATENT_CODE_SIZE = 512
 
 TILES = (3, 3)
 
+generator = StyledGenerator(LATENT_CODE_SIZE).to(device)
+generator.load_state_dict(torch.load('checkpoint/train_step-7.model')['g_running'])
+generator.eval()
 
-def get_spline():
-    codes = standard_normal_distribution.sample((SAMPLE_COUNT + 1, LATENT_CODE_SIZE)).numpy()
+@torch.no_grad()
+def get_spline(use_styles=True):
+    codes = standard_normal_distribution.sample((SAMPLE_COUNT + 1, LATENT_CODE_SIZE))
+    if use_styles:
+        codes = generator.style(codes.to(device))
+    
     codes[0, :] = codes[-1, :] # Make animation periodic
-    return CubicSpline(np.arange(SAMPLE_COUNT + 1), codes, axis=0, bc_type='periodic')
+    return CubicSpline(np.arange(SAMPLE_COUNT + 1), codes.detach().cpu().numpy(), axis=0, bc_type='periodic')
 
 def get_noise():
     noise = []
@@ -42,6 +50,7 @@ def get_noise():
 splines = [get_spline() for i in range(TILES[0] * TILES[1])]
 noises = [get_noise() for i in range(TILES[0] * TILES[1])]
 
+@torch.no_grad()
 def create_image_sequence():
     frame_index = 0
     progress_bar = tqdm(total=SAMPLE_COUNT * TRANSITION_FRAMES)
@@ -52,7 +61,8 @@ def create_image_sequence():
             images = []
             for spline, noise in zip(splines, noises):
                 code = torch.tensor(spline(float(sample_index) + step / TRANSITION_FRAMES), dtype=torch.float32, device=device).reshape(1, -1)
-                images.append(generator(code, noise=noise, step=STEP, alpha=1, mean_style=mean_style, style_weight=0.7))
+                image = generator.generator([code], noise=noise, step=STEP, alpha=1)
+                images.append(image)
 
             result = torch.zeros((3, TILES[0] * RESOLUTION, TILES[1] * RESOLUTION), device=device)
             p = 0
@@ -69,10 +79,6 @@ def create_image_sequence():
     print("\n\nUse this command to create a video:\n")
     print('ffmpeg -framerate 30 -i images/frame-%05d.png -c:v libx264 -profile:v high -crf 19 -pix_fmt yuv420p video.mp4')
 
-
-generator = StyledGenerator(LATENT_CODE_SIZE).to(device)
-generator.load_state_dict(torch.load('checkpoint/train_step-7.model')['g_running'])
-generator.eval()
 
 mean_style = get_mean_style(generator, device)
 create_image_sequence()
